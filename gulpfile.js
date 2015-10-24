44c0c08b5953f68f5894ae9637b58507370a2cb5
Daryl Hernandez <daryl650+github@gmail.com>
2015-10-24 16:39:55 +1100

    configured gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,52 @@
+// grab gulp packages
 var gulp = require('gulp'),
     gutil = require('gulp-util');
+
+// Include our plugins
+// General
+var concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+// CSS
+var sass = require('gulp-sass');
+
+// Concatenate and minify JS
+var jshint = require('gulp-jshint');
+
+
+// Lint task
+gulp.task('lint', function() {
+  return gulp.src('source/javscript/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+})
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src('source/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/assets/stylesheets'));
+})
+
+// Concat & Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('source/javascript/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('public/assets/javascript'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
+
+// Watch files for changes
+
+// Default task
 
 
 gulp.task('default', function() {
   return gutil.log('Gulp is running!');
 });
+
+
+gulp.watch('');
---
+++ public/assets/javascript/all.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  
+  
+  var people = [
+//     {
+//       firstName: 'Michael',
+//       lastName: 'Jackson'
+//     },
+//     {
+//       firstName: 'Mike',
+//       lastName: 'Monroe'
+//     }
+  ];
+  
+
+  // init
+  var template = Handlebars.compile( $("#peopleTemplate").html() );
+  var temp = template(people);
+  $('#people').append(temp);
+  
+  
+  // add people  
+  $('.myButton').on('click', function() {
+    var name = $(this).prev().val();
+    if (name) {
+      var newName = {
+        firstName: name.split(" ")[0],
+        lastName: name.split(" ")[1]
+      };
+    
+      people.push(newName);
+    
+      var newPeople = [newName];
+   
+      $(this).prev().val('');
+      $('#people').append(template(newPeople));
+      console.log(people);
+    }    
+  });
+
+  // delete people
+  
+  $('#people').on('click', '.del', function(e) {
+    console.log('hey');
+    var $remove = $(e.target).closest('li');
+    var i = $(this).find('ul').find('li').index($remove);
+    $remove.remove();
+    people.splice(i, 1);
+    console.log(people);
+  });
+});
