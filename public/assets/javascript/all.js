72423b6b76137bf97099de02ae23b3ca3a5c13d3
Daryl Hernandez <daryl650+github@gmail.com>
2015-10-27 18:19:42 +1100

    removed comments

--- public/assets/javascript/all.js
+++ public/assets/javascript/all.js
@@ -38,40 +38,4 @@ $(document).ready(function() {
   };
 
   people.init();
-
-  // var people = [];  
-
-  // // init
-  // var template = Handlebars.compile( $("#peopleTemplate").html() );
-  // var temp = template(people);
-  // $('#people').append(temp);  
-  
-  // // add people  
-  // $('.myButton').on('click', function() {
-  //   var name = $(this).prev().val();
-  //   if (name) {
-  //     var newName = {
-  //       firstName: name.split(" ")[0],
-  //       lastName: name.split(" ")[1]
-  //     };
-    
-  //     people.push(newName);
-    
-  //     var newPeople = [newName];
-   
-  //     $(this).prev().val('');
-  //     $('#people').append(template(newPeople));
-  //     console.log(people);
-  //   }    
-  // });
-
-  // // delete people  
-  // $('#people').on('click', '.del', function(e) {
-  //   console.log('hey');
-  //   var $remove = $(e.target).closest('li');
-  //   var i = $(this).find('ul').find('li').index($remove);
-  //   $remove.remove();
-  //   people.splice(i, 1);
-  //   console.log(people);
-  // });
 });
